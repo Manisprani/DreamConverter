86608514374774912492ec94cb706330241f0c4d
Emil Jönsson <ungothar97@gmail.com>
2020-11-01 16:22:25 +0100

    Data model suggestion for storing data from json file

---
+++ jsondata.py
@@ -0,0 +1,19 @@
+class JsonData(object):
+
+    def __init__(self, posX, posY, posZ, svgCode, svgType):
+        self.posX = posX
+        self.posY = posY
+        self.posZ = posZ
+
+        self.svgCode = svgCode
+        self.svgType = svgType
+
+def getPosition(self):
+    tempList = [self.posX,self.posY,self.posZ]
+    return tempList
+
+def getCode(self):
+    return self.svgCode
+
+def getType(self):
+    return self.svgType    

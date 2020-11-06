20c4e3a17a0c5ed649c172aca4aaf91fdae68a74
Emil Jönsson <ungothar97@gmail.com>
2020-11-06 14:44:54 +0100

    svg-viewbox now is equal to dxf-viewbox

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from libredwg import *
 from ezdxf import recover
 from convertDXF import *
 
-print_to_log_file = True    # Whether print statements should print to a log.txt file, or in console.
+print_to_log_file = False    # Whether print statements should print to a log.txt file, or in console.
 
 ### I/O ###                         # TODO to be decided by Vectorworks API
-dwg_file = 'ullaoriginal.dwg'       # TODO currently not used. Vectorworks API will load chosen DWG, export it to DXF, and use the exported DXF as it's intermediary file
-dxf_file = 'ullaoriginal.dxf'
-svg_file = 'ullaoriginal.svg'
+dwg_file = 'FILE GO HERE.dwg'       # TODO currently not used. Vectorworks API will load chosen DWG, export it to DXF, and use the exported DXF as it's intermediary file
+dxf_file = 'FILE GO HERE.dxf'
+svg_file = 'FILE GO HERE.svg'
 
 
 ### Constants ###                   # TODO to be decided by the Vectorworks API
@@ -16,6 +16,10 @@ LIBREDWG_DIR = pathlib.Path(__file__).parent.joinpath('libredwg').absolute()
 DWG_PATH = LIBREDWG_DIR.joinpath(dwg_file).absolute()
 DXF_PATH = LIBREDWG_DIR.joinpath(dxf_file).absolute()
 SVG_PATH = LIBREDWG_DIR.joinpath(svg_file).absolute()
+
+def print_entity(e):
+    print("start point: %s\n" % e.dxf.start)
+    print("end point: %s\n" % e.dxf.end)
 
 
 ### PROGRAM ###
@@ -25,6 +29,12 @@ if print_to_log_file:
 try:
     doc = ezdxf.readfile(DXF_PATH)      # some dxfs from libredwg are broken, and instead needs recover.readfile(...), which seems to delete the INSERTs...
     msp = doc.modelspace()
+    maxPosition = (doc.header['$EXTMAX'])
+    minPosition = (doc.header['$EXTMIN']) # finds the bounds of the dxf
+    print (doc.header['$EXTMAX'])
+    print (doc.header['$EXTMIN'])
+
+  
 
     svg = svgwrite.Drawing(filename=SVG_PATH, debug=True)
     for e in msp:
@@ -32,7 +42,7 @@ try:
             convert_recursively(e.virtual_entities(), svg)
         else:
             convert_entity(e, svg)
-    svg.viewbox(0, 0, 100000, 100000)                                   # TODO identify DWG document bounds and assign them here instead
+    svg.viewbox(minPosition[0],minPosition[1], maxPosition[0] + abs(minPosition[0]), maxPosition[1] + abs(minPosition[1]))   
     svg.save()
 except IOError:
     print(f'Not a DXF file or a generic I/O error.')

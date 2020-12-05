6ac228f52654b7f29c9c9e6744cbebeb5f71e88d
Nilsiker <nilsiker94@gmail.com>
2020-12-05 15:19:18 +0100

    redid main.py to work with new VW script interface

--- main.py
+++ main.py
@@ -5,35 +5,31 @@ import svgbuilder
 from convertDXF import *
 
 
-def run(input_file: str, output_path=None, debug: bool = False):
-    if output_path:
-        output_path = output_path[1]
-
-    if debug:
-        DEBUG_DIR = pathlib.Path(__file__).parent.joinpath('files').absolute()
-        DXF_PATH = DEBUG_DIR.joinpath(input_file).absolute()
-        SVG_PATH = DEBUG_DIR.joinpath(
-            input_file.replace('.dxf', '.svg')).absolute()
-    else:
-        DXF_PATH = pathlib.Path(input_file).absolute()
-        if output_path:
-            SVG_PATH = pathlib.Path(output_path).joinpath(
-                DXF_PATH.name.replace('.dxf', '.svg'))
-        else:
-            SVG_PATH = input_file.replace('.dxf', '.svg')
-
-    ### PROGRAM ###
-    try:
-        svgbuilder.build(DXF_PATH, SVG_PATH)
-    except IOError:
-        print(f'Not a DXF file or a generic I/O error.')
-        sys.exit(1)
-    except ezdxf.DXFStructureError:
-        print(f'Invalid or corrupted DXF file.')
-        sys.exit(2)
+debug: bool = False
+input_file = "export99.dxf"
 
 
-run('export99.dxf', None, True)
+if debug:
+    DEBUG_DIR = pathlib.Path(__file__).parent.joinpath('files').absolute()
+    DXF_PATH = DEBUG_DIR.joinpath(input_file).absolute()
+    SVG_PATH = DEBUG_DIR.joinpath(
+        input_file.replace('.dxf', '.svg')).absolute()
+else:
+    DXF_PATH = pathlib.Path(sys.argv[1]).absolute()
+    SVG_PATH = pathlib.Path(sys.argv[2]).absolute()
+
+### PROGRAM ###
+# print(DXF_PATH)
+# print(SVG_PATH)
+try:
+    svgbuilder.build(DXF_PATH, SVG_PATH)
+except IOError:
+    print(f'Not a DXF file or a generic I/O error.')
+    # sys.exit(1)
+except ezdxf.DXFStructureError:
+    print(f'Invalid or corrupted DXF file.')
+    # sys.exit(2)
+
 
 # vi kan ha excepts i vs-skriptet,
 # och visa alert dialog om IOError
